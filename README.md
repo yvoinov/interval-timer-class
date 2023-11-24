18d7bd58982adfeaa600b3c3f986a06041c13fc1
Yuri Voinov <yvoinov@gmail.com>
2023-11-25 00:17:17 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Interval timer class
+[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://github.com/yvoinov/interval-timer-class/blob/master/LICENSE)
+
+Easy to use, Boost-free interval timer class for periodic tasks that need to be performed constantly.
+
+To use just include this header to your project and instantiate class as ahown:
+
+```cpp
+// Instantiation
+Timer<std::size_t, std::function<void()>> timer(/* Here must be timer interval */, []() { /* Executable statements */ });
+```
---
+++ interval_timer_class.cc
@@ -0,0 +1,32 @@
+#include <cstdlib>	/* For std::size_t */
+#include <chrono>
+#include <atomic>
+#include <condition_variable>
+#include <mutex>
+#include <thread>
+#include <functional>	/* For std::function */
+
+/* Timer class with on-load init */
+/* Intended to run a given block (lambda) on a periodic basis at a given interval */
+/* Templated version */
+
+template <typename T, typename F>
+class Timer {
+public:
+	Timer(T p_interval, F p_exec) : m_interval(p_interval), m_exec(p_exec) {
+		std::thread([&]() { while (m_running.load(std::memory_order_relaxed)) {
+					std::unique_lock<std::mutex> tlock(m_conditional_mutex);
+					if (!m_conditional_lock.wait_for(tlock, std::chrono::seconds(m_interval),
+						[this]() { return !m_running.load(std::memory_order_acquire); }))
+						m_exec();
+				}
+		}).detach();
+	}
+	~Timer() { m_running.store(false, std::memory_order_release); m_conditional_lock.notify_one(); }
+private:
+	T m_interval;
+	F m_exec;
+	std::atomic<bool> m_running { true };
+	std::condition_variable m_conditional_lock;
+	std::mutex m_conditional_mutex;
+};
